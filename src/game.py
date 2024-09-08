87e6e3c6a46d93608f8ea40a584eaef011b68662
sxx3306 <dascorf@gmail.com>
2024-09-08 18:53:47 -0400

    First test of the game

--- src/game.py
+++ src/game.py
@@ -12,24 +12,28 @@ class Sprite(pygame.sprite.Sprite):
         self.color = color
         self.width = width
         self.height = height
+        self.start = False
 
     def handle_click(self, event):
         if event.type == pygame.MOUSEBUTTONUP and self.rect.collidepoint(event.pos):
             self.on_click()
 
     def on_click(self):
-        print("Button clicked!")
+        if self.start: 
+            self.start = False
+            self.image.fill('GREEN')
+            self.image.set_alpha(255)
+        else:
+            self.start = True
+            self.image.set_alpha(0)
+
+        return True
 
     def blit(self, screen):
         screen.blit(self.image, self.rect)
 
-        
-class Game():
-    """
 
-    
-
-    """
+class Game:
     def __init__(self, grid: tuple[int, int], resolution: tuple[float, float] = (800.0, 600.0), cell_size: int = 10):
         pygame.init()
 
@@ -40,53 +44,68 @@ class Game():
         self.screen = pygame.display.set_mode(self.res)
         self.clock = pygame.time.Clock()
 
-        self.start_button = Sprite('GREEN', (self.res[0] / len(self.grid)) * 2, self.res[0], (self.res[0] - (self.res[0] / len(self.grid)) * 2, self.res[1] - (self.res[0] / len(self.grid)) * 2))
+        self.start_button = Sprite('GREEN', self.res[0] / len(self.grid) * 2, self.res[0], (self.res[0] / len(self.grid) * (len(self.grid) - 2), 0))
         self.running = False
 
     def check(self, index: list[int, int]):
         count = 0
+        rows, cols = len(self.grid), len(self.grid[0])
 
-        for i in range(index[0] - 1, index[0] + 1):
-            for j in range(index[1] - 1, index[1] + 1):
-                try:
-                    if [i, j] != index and self.grid[i][j] == 1:
-                        count += 1
-                except IndexError:
-                    pass
-        
+        for i in range(index[0] - 1, index[0] + 2):
+            for j in range(index[1] - 1, index[1] + 2):
+                if (i == index[0] and j == index[1]) or i < 0 or i >= rows or j < 0 or j >= cols:
+                    continue
+                count += self.grid[i][j]
+
         return count
+
+    def update_grid(self):
+        new_grid = np.copy(self.grid)
+        rows, cols = len(self.grid), len(self.grid[0])
+
+        for row in range(rows):
+            for col in range(cols):
+                neighbors = self.check([row, col])
+
+                if self.grid[row][col] == 1:
+                    if neighbors < self.rules[0] or neighbors > self.rules[1]:
+                        new_grid[row][col] = 0
+                else:
+                    if neighbors == self.rules[1]:
+                        new_grid[row][col] = 1
+
+        self.grid = new_grid
 
     def run(self):
         while True:
+            self.running = self.start_button.start
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     exit()
 
                 if event.type == pygame.MOUSEBUTTONUP:
-                    self.start_button.handle_click(event)
-                    pos = event.pos
-                    row, col = int(pos[1] / (self.res[1] / len(self.grid[0]))), int(pos[0] / (self.res[0] / len(self.grid)))
-
-                    if self.grid[row][col] == 1:
-                        self.grid[row][col] = 0
+                    if self.start_button.handle_click(event):
+                        pass
                     else:
-                        self.grid[row][col] = 1
+                        pos = event.pos
+                        row, col = int(pos[1] / (self.res[1] / len(self.grid[0]))), int(pos[0] / (self.res[0] / len(self.grid)))
+
+                        if self.grid[row][col] == 1:
+                            self.grid[row][col] = 0
+                        else:
+                            self.grid[row][col] = 1
 
             self.screen.fill('black')
 
             for i in range(1, len(self.grid)):
                 pygame.draw.line(self.screen, 'white', (i * (self.res[0] / len(self.grid)), 0), (i * (self.res[0] / len(self.grid)), self.res[1]))
-                pygame.draw.line(self.screen, 'white', (0, i * (self.res[1] / len(self.grid))), (self.res[0] , i * (self.res[1] / len(self.grid))))
+                pygame.draw.line(self.screen, 'white', (0, i * (self.res[1] / len(self.grid))), (self.res[0], i * (self.res[1] / len(self.grid))))
 
             width, height = self.res[0] / len(self.grid), self.res[1] / len(self.grid[0])
 
             if self.running:
-                for row in range(len(self.grid)):
-                    for col in range(len(self.grid[row])):
-                        if self.check([row, col]) == self.rules[1]:
-                            self.grid[row][col] == 1
-                        elif self.check([row, col]) < self.rules[0] or self.check([row, col]) > self.rules[0]:
-                            self.grid[row][col] == 0
+                self.update_grid()
 
             for row in range(len(self.grid)):
                 for col in range(len(self.grid[row])):
@@ -97,9 +116,8 @@ class Game():
 
             self.start_button.blit(self.screen)
             pygame.display.flip()
-            self.clock.tick(60)
+            self.clock.tick(5)
 
 if __name__ == '__main__':
     game = Game((20, 20))
-
     game.run()
